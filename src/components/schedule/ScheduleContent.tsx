d433655aadb05b11ef808c5d3c28a07dac4ebc16
Timothy Yang <timothyyangemail@gmail.com>
2026-05-19 23:40:44 +1000

    try update vercel

--- src/components/schedule/ScheduleContent.tsx
+++ src/components/schedule/ScheduleContent.tsx
@@ -18,7 +18,7 @@ export default function ScheduleContent() {
     const fetchSocietyData = async () => {
       const url = "https://api.hellorubric.com/";
       const detailsObj = {
-        societyid: "6335",
+        societyid: "6334",
         domain: "campus.hellorubric.com",
       };
 
