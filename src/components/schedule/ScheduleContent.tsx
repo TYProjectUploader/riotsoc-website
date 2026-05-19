3524d3f36ca927a2beb5fb24f046e7f587bf1979
Timothy Yang <timothyyangemail@gmail.com>
2026-05-19 23:40:53 +1000

    v

--- src/components/schedule/ScheduleContent.tsx
+++ src/components/schedule/ScheduleContent.tsx
@@ -18,7 +18,7 @@ export default function ScheduleContent() {
     const fetchSocietyData = async () => {
       const url = "https://api.hellorubric.com/";
       const detailsObj = {
-        societyid: "6334",
+        societyid: "6335",
         domain: "campus.hellorubric.com",
       };
 
